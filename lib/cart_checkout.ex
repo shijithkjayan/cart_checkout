ae0833623a9458568e453dce40b5750b800e2b9e
Shijith K <shijithjayan@gmail.com>
2025-03-05 19:30:56 +0530

    :tada: initial commit

---
+++ lib/cart_checkout.ex
@@ -0,0 +1,18 @@
+defmodule CartCheckout do
+  @moduledoc """
+  Documentation for `CartCheckout`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CartCheckout.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cart_checkout_test.exs
@@ -0,0 +1,8 @@
+defmodule CartCheckoutTest do
+  use ExUnit.Case
+  doctest CartCheckout
+
+  test "greets the world" do
+    assert CartCheckout.hello() == :world
+  end
+end
